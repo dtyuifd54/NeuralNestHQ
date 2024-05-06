9785887f6c21c0ba3ee0afef096e1341d37e4a4a
dtyuifd54 <isjctshaleyxoxo@hotmail.com>
2024-05-06 23:55:21 +0700

    feat project

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const pullAtIndex = (arr, pullArr) => {
-  let removed = [];
-  let pulled = arr
-    .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
-    .filter((v, i) => !pullArr.includes(i));
-  arr.length = 0;
-  pulled.forEach((v) => arr.push(v));
-  return removed;
-};
+const isPalindrome = (str) => str === str.split("").reverse().join("");
